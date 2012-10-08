bd2bdff01c118263533efdf7dfff8777b1e0efee
Langel <knoxburry@gmail.com>
2012-10-08 14:35:09 -0700

    added config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+# database crap
+
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+
+# smtp crap
+
+
+
+# openID, OAuth, and API keys
+
+?>
