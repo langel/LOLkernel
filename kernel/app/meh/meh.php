0945d267d3ce2fac5a7c9e4dbda5237871fc681e
Langel <knoxburry@gmail.com>
2012-12-13 18:50:58 -0800

    meh == MEdia Heuristics and assets_media

---
+++ kernel/app/meh/meh.php
@@ -0,0 +1,17 @@
+/*
+
+
+	M E H stands for
+
+	MEdia Heuristics
+
+
+	.htaccess looks for /meh/* fill calls 
+	first it looks in /app/assets_media/
+// XXX the following are not completely implemented!??! :U
+	then it looks in /kernel/app/assets_media/
+	then LOLkernel looks for controller /app/meh/meh.php
+	lastly, this controller is ran and we get a 404
+
+
+*/
